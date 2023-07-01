f06874d4b1ccf733a05d47f63d4b5087de4b30e5
AykutIpek <aykutipek_2001@hotmail.com>
2023-07-01 13:19:47 +0300

    Initial Commit

---
+++ IpekAykutGmail/IpekAykutGmailApp.swift
@@ -0,0 +1,17 @@
+//
+//  IpekAykutGmailApp.swift
+//  IpekAykutGmail
+//
+//  Created by aykut ipek on 1.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct IpekAykutGmailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
